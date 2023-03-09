024f3265eaec95226a3bef8c2c2daed157a5586b
SergioTanakaAtos <sergio.tanaka@atos.net>
2023-03-09 13:10:02 -0300

    classmethod

--- seccao5/aula1.py
+++ seccao5/aula1.py
@@ -2,6 +2,7 @@
 # São métodos onde "self" será "cls", ou seja,
 # ao invés de receber a instância no primeiro
 # parâmetro, receberemos a própria classe.
+
 class Pessoa:
     ano = 2023  # atributo de classe
 
