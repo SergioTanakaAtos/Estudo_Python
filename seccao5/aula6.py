603c553b97070fd283e7707f20d82d52acaeb8ee
SergioTanakaAtos <sergio.tanaka@atos.net>
2023-03-13 14:19:21 -0300

    OtherExceptions

---
+++ seccao5/aula6.py
@@ -0,0 +1,24 @@
+
+
+class MyError(Exception):
+    ...
+
+
+class OtherError(Exception):
+    ...
+
+def levantar():
+    exception_ = MyError("A mensagem do meu erro")
+    exception_ .add_note("voce errou isso")
+    raise exception_
+
+try:
+    levantar()
+except MyError as error:
+    print(error.__class__.__name__)
+    print(error)
+    print()
+    exception_ = OtherError("A mensagem do meu outro erro")
+    exception_.add_note("mais uma nota")
+    exception_.__notes__ += error.__notes__.copy()
+    raise exception_ from error 
